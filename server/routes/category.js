ab7ce1be68aaf65052c46babb8ce47562b2c8b6d
ThanhTuan1607 <nguyentuan1235868@gmail.com>
2024-03-27 17:37:13 +0700

    99,9%

--- server/routes/category.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router()
-const ctrls = require('../controllers/category')
-
-
-router.post('/createCategory',ctrls.createCategory)
-router.get('/',ctrls.getAllCategory)
-router.delete('/',ctrls.deleteCategory)
-
-module.exports = router
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,8 @@
 const { notFound, errHandle } = require('../middlewares/errHandle')
-const actorRouter = require("./actor")
-const categoryRouter = require('./category')
 const movieRouter = require('./movie')
 const movieEpisodeRouter = require('./movieEpisode')
 
 const initRoutes = (app) => {
-    app.use("/api/actor", actorRouter)
-    app.use("/api/category", categoryRouter)
     app.use("/api/movie", movieRouter)
     app.use("/api/movieEpisode", movieEpisodeRouter)
 
